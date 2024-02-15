7e938ff23718b42738eb1dec85370f5101275e09
Vukosavljev <vukosavljevs@gmail.com>
2024-02-15 14:19:02 +0100

    feat: add test mapper

--- jest.config.ts
+++ jest.config.ts
@@ -8,4 +8,11 @@ module.exports = {
   resetMocks: true,
   restoreMocks: true,
   coverageReporters: ['html', 'lcov', 'text'],
+  moduleNameMapper: {
+    '^@routes': '<rootDir>/src/routes/$1',
+    '^@controllers': '<rootDir>/src/controllers/$1',
+    '^@constants': '<rootDir>/src/constants/$1',
+    '^@utils': '<rootDir>/src/utils/$1',
+    '^@validators': '<rootDir>/src/validators/$1',
+  },
 };
