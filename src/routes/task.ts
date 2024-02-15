41479ebd7ac70c26d21b381450ac03daacec4a9d
Vukosavljev <vukosavljevs@gmail.com>
2024-02-15 14:34:40 +0100

    feat: validate task title

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
 import * as TaskControllers from '@controllers';
-import { isAuthenticated } from '../middlewares';
-// import { titleValidators } from '@validators';
+import { isAuthenticated } from '@middlewares';
+import { titleValidators } from '@validators';
 
 const router = Router();
 
 router.get('/:id', isAuthenticated, TaskControllers.getTask);
 router.get('/', isAuthenticated, TaskControllers.getTasks);
-router.post('/', [isAuthenticated], TaskControllers.createTask);
-router.patch('/:id', isAuthenticated, TaskControllers.updateTask);
+router.post(
+  '/',
+  [isAuthenticated, titleValidators],
+  TaskControllers.createTask
+);
+router.patch(
+  '/:id',
+  [isAuthenticated, titleValidators],
+  TaskControllers.updateTask
+);
 router.delete('/:id', isAuthenticated, TaskControllers.deleteTask);
 
 export default router;
