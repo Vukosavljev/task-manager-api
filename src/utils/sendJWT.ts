20fcf0bf00e2573a3c59263b40c60f67265727cd
Vukosavljev <vukosavljevs@gmail.com>
2024-02-17 22:58:15 +0100

    refactor: send jwt

--- src/utils/sendJWT.ts
+++ src/utils/sendJWT.ts
@@ -6,15 +6,14 @@ export const sendToken = (
   statusCode: number,
   res: Response
 ) => {
+  const { COOKIE_EXPIRES_TIME, NODE_ENV } = process.env;
   const token = user.getJwtToken();
   const options: CookieOptions = {
-    expires: new Date(
-      Date.now() + +process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + +COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
     httpOnly: true,
   };
 
-  if (process.env.NODE_ENV === 'prod') options.secure = true;
+  if (NODE_ENV === 'prod') options.secure = true;
 
   res.status(statusCode).cookie('token', token, options).json({
     success: true,
